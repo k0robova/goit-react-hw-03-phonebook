a1bbdc6fdd656461f3e6feb23648e79b6bf6b295
k0robova <alonakorobova@ukr.net>
2023-11-14 22:44:37 +0200

    localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,18 @@ import css from './App.module.css';
 import { Form } from './Form/Form';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
+import { paste } from '@testing-library/user-event/dist/paste';
 
 export class App extends React.Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
-
-  // generateId = () => nanoid();
 
   formSubmitHandler = data => {
     const { name, number } = data;
@@ -30,7 +29,6 @@ export class App extends React.Component {
       return;
     } else {
       const newContact = {
-        // id: this.generateId(),
         id: nanoid(),
         name: name,
         number: number,
@@ -56,6 +54,20 @@ export class App extends React.Component {
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
 
   render() {
     return (
